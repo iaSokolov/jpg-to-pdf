08d360586d3f42b86735594066fc7f3f628ef0d9
iaSokolov <iasokolov@gmail.com>
2021-01-12 17:05:03 +0300

    собираем файлы

--- src/main/java/jpgtopdf/App.java
+++ src/main/java/jpgtopdf/App.java
@@ -5,33 +5,58 @@ import com.itextpdf.text.DocumentException;
 import com.itextpdf.text.Image;
 import com.itextpdf.text.PageSize;
 import com.itextpdf.text.pdf.PdfWriter;
+import org.apache.commons.io.FilenameUtils;
 
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.lang.reflect.Array;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class App {
+    // первый параметр - источник с какой папки надо смотреть файлики
+    // второй параметр - куда надо складывать результаты
     public static void main(String[] args) throws IOException, DocumentException {
-        File root = new File("<folder containing images>");
-        String outputFile = "output.pdf";
-        List<String> files = new ArrayList<String>();
-        files.add("page1.jpg");
-        files.add("page2.jpg");
+        System.out.println("start-of-program");
+
+        String outputPath = "";
+        String rootPath = "";
+        if (args != null && args.length == 2) {
+            outputPath = args[0];
+            rootPath = args[1];
+            System.out.println("error");
+        }
+
+        if (outputPath.isEmpty() || rootPath.isEmpty()) {
+            System.out.println("error");
+        }
+
+        File root = new File(rootPath);
+        String outputFile = String.format("%s/%s.pdf", outputPath, root.getName());
+
+        List<String> fileNameList = Arrays.stream(root.listFiles())
+                .filter(file -> FilenameUtils.getExtension(file.getName()).equals("jpg"))
+                .map(file -> file.getAbsolutePath())
+                .sorted(String::compareTo)
+                .collect(Collectors.toList());
 
         Document document = new Document();
         PdfWriter.getInstance(document, new FileOutputStream(new File(root, outputFile)));
         document.open();
-        for (String f : files) {
+
+        for (String fileName : fileNameList) {
             document.newPage();
-            Image image = Image.getInstance(new File(root, f).getAbsolutePath());
+            Image image = Image.getInstance(fileName);
+            image.scaleToFit(PageSize.A4);
+            image.setBorderWidth(1);
             image.setAbsolutePosition(0, 0);
-            image.setBorderWidth(0);
-            image.scaleAbsolute(PageSize.A4);
             document.add(image);
         }
         document.close();
+        System.out.println("end-of-program");
     }
 }
